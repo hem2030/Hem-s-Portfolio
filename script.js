27c0993192c58c7947206e8fd91b8f9d7d1f916e
hem2030 <hrmehta2008@gmail.com>
2026-09-05 12:40:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hem Mehta Portflio Loaded Succesfully !");
+
+const year = new
+Date().getFullYear();
+
+document.queryZelector("footerp").innerHTM = `© ${year} Hem Mehta.All Rights Reserved.s`
